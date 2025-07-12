da7be71ba5759d7dfeb8a09835cf359d47515ff4
iamsyedbilal <syedbilal.dev27@gmail.com>
2025-07-13 00:21:26 +0500

    problem_1 solved

---
+++ problem_7/problem_7.js
@@ -0,0 +1,16 @@
+function filter(arr, fn) {
+  const filteredArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) {
+      filteredArr.push(arr[i]);
+    }
+  }
+
+  return filteredArr;
+}
+
+const greaterThan10 = function (n) {
+  return n > 10;
+};
+filter([0, 10, 20, 30], greaterThan10);
